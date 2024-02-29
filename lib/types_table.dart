9625d0ede8f669259bffb9ecd3b7a30cfadf4968
Alvaro <alvaromagu2002@gmail.com>
2024-02-29 23:20:46 +0100

    change chips filter

--- lib/types_table.dart
+++ lib/types_table.dart
@@ -13,43 +13,67 @@ class TypesTablePage extends StatefulWidget {
 }
 
 class _TypesTablePageState extends State<TypesTablePage> {
-  List<String> selectedTypes = [];
+  List<String> selectedStrengths = [];
 
   @override
   void initState() {
     super.initState();
-    selectedTypes = widget.selectedTypes ?? [];
+    selectedStrengths = widget.selectedTypes ?? [];
   }
 
   @override
   Widget build(BuildContext context) {
     final lowercaseText = widget.searchText.toLowerCase();
+    int chipIndex = 0;
+    pokemonTypes.sort((a, b) {
+      final aSelected = selectedStrengths.contains(a.name.toLowerCase());
+      final bSelected = selectedStrengths.contains(b.name.toLowerCase());
+      if (aSelected && !bSelected) {
+        return -1;
+      } else if (!aSelected && bSelected) {
+        return 1;
+      }
+      return a.name.compareTo(b.name);
+    });
 
     return SingleChildScrollView(
       child: Column(
         children: [
-          if (selectedTypes.isNotEmpty)
-            Wrap(
-              spacing: 5,
-              children: selectedTypes.map((type) {
-                final typeColors = typeColorMap[type]!;
-                return Padding(
-                  padding: const EdgeInsets.all(0),
-                  child: InputChip(
-                    label: Text(t(type)),
-                    padding: EdgeInsets.zero,
-                    labelStyle: TextStyle(color: Color(typeColors['textColor']!)),
-                    backgroundColor: Color(typeColors['color']!),
-                    onDeleted: () {
-                      setState(() {
-                        selectedTypes = selectedTypes.where((element) => element != type).toList();
-                      });
-                    },
-                  ),
-                );
-              }).toList(),
+          const Text('Fortalezas', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: pokemonTypes.expand((type) {
+                  chipIndex++;
+                  final typeColors = typeColorMap[type.name.toLowerCase()]!;
+                  final selected = selectedStrengths.contains(type.name.toLowerCase());
+
+                  return [
+                    if (chipIndex != 1) const SizedBox(width: 5),
+                    InputChip(
+                      label: Text(t(type.name)),
+                      padding: EdgeInsets.zero,
+                      labelStyle: TextStyle(color: Color(typeColors['textColor']!)),
+                      backgroundColor: Color(typeColors['color']!),
+                      onSelected: (bool selected) {
+                        setState(() {
+                          if (selected) {
+                            selectedStrengths.add(type.name.toLowerCase());
+                          } else {
+                            selectedStrengths.remove(type.name.toLowerCase());
+                          }
+                        });
+                      },
+                      selected: selected,
+                      avatar: selected ? null : const SizedBox(width: 20),
+                    )
+                  ];
+                }).toList()
+              ),
             ),
-          
+          ),
           DataTable(
             columnSpacing: 15,
             dataRowMaxHeight: double.infinity,
@@ -80,11 +104,11 @@ class _TypesTablePageState extends State<TypesTablePage> {
               ),
             ],
             rows: pokemonTypes.where((type) {
-                  final typeLowered =  type.name.toLowerCase();
-                  if (selectedTypes.isNotEmpty) {
-                    return selectedTypes.contains(typeLowered) && typeLowered.contains(lowercaseText);
-                  }
-                  return typeLowered.contains(lowercaseText);
+                  final typeLowered = type.name.toLowerCase();
+                  final translation = t(typeLowered).toLowerCase();
+                  final strengthsCondition = selectedStrengths.isEmpty || 
+                    type.strengths.any((e) => selectedStrengths.contains(e.toLowerCase()));
+                  return strengthsCondition && translation.contains(lowercaseText);
                 }).map((type) {
                   return DataRow(
                     cells: [
